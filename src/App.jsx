cb5ede72a5c4ba1810f2701d27be7b8ababbb3f0
“Robert <kulewakangitsirobert@gmail.com>
2026-02-02 11:52:10 +0200

    chore: initialize project with Vite + React + TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='bg-gray-800 text-xl text-white p-4 rounded-lg'>Weather App</h1>
+    </>
+  )
+}
+
+export default App
